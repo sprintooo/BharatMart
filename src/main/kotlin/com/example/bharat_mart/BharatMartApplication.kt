35e16d4f6b722e1fee821e7dd7ac8e40f30e1134
Ipsita Behera <sprinto>
2025-03-01 16:33:30 +0530

    initial commit

---
+++ src/main/kotlin/com/example/bharat_mart/BharatMartApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bharat_mart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BharatMartApplication
+
+fun main(args: Array<String>) {
+	runApplication<BharatMartApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bharat_mart/BharatMartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bharat_mart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BharatMartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
